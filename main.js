4db21a4bcb5304d365b296f2831074bf935b6255
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-04-16 17:01:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/EWayhQl8D/model.json',modelLoaded);
 
   function modelLoaded() {
     console.log('Model Loaded!');
